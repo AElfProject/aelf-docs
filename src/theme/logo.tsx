9f9c34811218db94ee5665494fe36fc7b1be6482
hzz780 <hzz780@qq.com>
2024-07-23 20:57:27 +0800

    Feat/chaingpt (#145)
    * feat: 🎸 chaingpt demo
    
    * refactor: 💡 move chainGPT to src/theme; upgrade to 0.1.1
    
    * ci: 🎡 update chaingpt-component
    
    * feat: 🎸 chaingpt/api/chat -> /api/chaingpt

--- src/theme/logo.tsx
+++ src/theme/logo.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ThemedImage from "@theme/ThemedImage";
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import ChainGPT from '@site/src/theme/ChainGPT';
 
 /**
  * Reference: https://docusaurus.io/docs/markdown-features/assets#themed-images
  */
 export default function Logo(): JSX.Element {
   return (
-    <ThemedImage
-      alt="aelf"
-      sources={{
-        light: useBaseUrl("/img/Logo.aelf.svg"),
-        dark: useBaseUrl("/img/Logo.aelf.white.svg"),
-      }}
-    />
+    <>
+      <ThemedImage
+        alt="aelf"
+        sources={{
+          light: useBaseUrl("/img/Logo.aelf.svg"),
+          dark: useBaseUrl("/img/Logo.aelf.white.svg"),
+        }}
+      />
+      <div>
+        <ChainGPT />
+      </div>
+    </>
   );
 }
