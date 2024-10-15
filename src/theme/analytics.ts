b820116626056ee72ad39d4eb57be53502a89552
yongen.loong <yongen.loong@aelf.io>
2024-10-15 15:42:08 +0800

    fix: run client side

--- src/theme/analytics.ts
+++ src/theme/analytics.ts
@@ -3,12 +3,14 @@ import { gaEventsForwarderPlugin } from "@amplitude/plugin-ga-events-forwarder-b
 import ReactGA from "react-ga4";
 import TagManager from "react-gtm-module";
 
-const gaEventsForwarder = gaEventsForwarderPlugin();
-amplitude.add(gaEventsForwarder);
+export const analytics = () => {
+  const gaEventsForwarder = gaEventsForwarderPlugin();
+  amplitude.add(gaEventsForwarder);
 
-amplitude.init("7652218546e8f6cc3d045e43a68830f6");
+  amplitude.init("7652218546e8f6cc3d045e43a68830f6");
 
-ReactGA.initialize("G-PFZ0BCQHMY");
-TagManager.initialize({
-  gtmId: "GTM-NKWDMQ52",
-});
+  ReactGA.initialize("G-PFZ0BCQHMY");
+  TagManager.initialize({
+    gtmId: "GTM-NKWDMQ52",
+  });
+};
