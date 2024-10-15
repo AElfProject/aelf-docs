bc6b40a8dbef9785197979417d18b86c1a3b9f98
yongen.loong <yongen.loong@aelf.io>
2024-10-15 15:58:35 +0800

    feat: session replay

--- src/theme/analytics.ts
+++ src/theme/analytics.ts
@@ -1,5 +1,6 @@
 import * as amplitude from "@amplitude/analytics-browser";
 import { gaEventsForwarderPlugin } from "@amplitude/plugin-ga-events-forwarder-browser";
+import { sessionReplayPlugin } from "@amplitude/plugin-session-replay-browser";
 import ReactGA from "react-ga4";
 import TagManager from "react-gtm-module";
 
@@ -7,6 +8,11 @@ export const analytics = () => {
   const gaEventsForwarder = gaEventsForwarderPlugin();
   amplitude.add(gaEventsForwarder);
   amplitude.init("7652218546e8f6cc3d045e43a68830f6", { autocapture: true });
+  const sessionReplayTracking = sessionReplayPlugin({
+    sampleRate: 0.01,
+  });
+  amplitude.add(sessionReplayTracking);
+
   ReactGA.initialize("G-PFZ0BCQHMY");
   TagManager.initialize({
     gtmId: "GTM-NKWDMQ52",
